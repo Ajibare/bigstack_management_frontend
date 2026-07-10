20bce47782161502d9d47b16ca21c97c21705b41
Ajibare <ajibare.jide@gmail.com>
2026-07-10 23:24:10 +0100

    not fully rready

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3001/api' });
+const api = axios.create({ baseURL: 'https://bigstack-management.vercel.app/api' });
+// const api = axios.create({ baseURL: 'http://localhost:3001/api' });
 
 export default api;
 
